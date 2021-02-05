5e4b73f93eb8de992a5811a68040c7363c77b6a8
David Muolo <davidmuolo1@gmail.com>
2021-02-05 15:12:29 -0600

    Commit2

--- PersonalSite1.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite1.UI.MVC/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace PersonalSite1.UI.MVC.Controllers
 
             return View();
         }
+
+        public ActionResult PortfolioDetails()
+        {
+            return View();
+        }
     }
 }
