eadf583c8130b7057450404e1c5ab2987520ac6e
David Muolo <davidmuolo1@gmail.com>
2021-02-11 22:48:57 -0600

    NewPush

--- PersonalSite1.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite1.UI.MVC/Controllers/HomeController.cs
@@ -13,23 +13,14 @@ namespace PersonalSite1.UI.MVC.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult PortfolioDetails()
         {
             return View();
         }
+
+        //public ActionResult Contact()
+        //{
+        //    return View();
+        //}
     }
 }
