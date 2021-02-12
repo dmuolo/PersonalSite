89461a9e5defee3b61e57c22d447a83c4157ba40
David Muolo <davidmuolo1@gmail.com>
2021-02-12 16:25:27 -0600

    NewCommit

--- PersonalSite1.UI.MVC/Controllers/HomeController.cs
+++ PersonalSite1.UI.MVC/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalSite1.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
 
 namespace PersonalSite1.UI.MVC.Controllers
 {
@@ -18,9 +21,30 @@ namespace PersonalSite1.UI.MVC.Controllers
             return View();
         }
 
-        //public ActionResult Contact()
-        //{
-        //    return View();
-        //}
+        [HttpPost]
+        public JsonResult ContactAjax(ContactViewModel cvm)
+        {
+            string body = $"{cvm.Name} has sent you the following message: <br />" + $"{cvm.Message} <strong>from the email address:</strong> {cvm.Email}";
+
+            MailMessage m = new MailMessage("admin@davidmuolo.com", "davidmuolo1@gmail.com", cvm.Subject, body);
+
+            m.IsBodyHtml = true;
+            m.Priority = MailPriority.High;
+            m.ReplyToList.Add(cvm.Email);
+
+            SmtpClient client = new SmtpClient("mail.davidmuolo.com");
+            client.Credentials = new NetworkCredential("admin@davidmuolo.com", "Stubble848!");
+
+            client.Port = 8889;
+            try
+            {
+                client.Send(m);
+            }
+            catch(Exception e)
+            {
+                ViewBag.Message = e.StackTrace;
+            }
+            return Json(cvm);
+        }
     }
 }
---
+++ PersonalSite1.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalSite1.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is Required *")]
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "* Email is Required *")]
+        public string Email { get; set; }
+
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is Required *")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+    }
+}
